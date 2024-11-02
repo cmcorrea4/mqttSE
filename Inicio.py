026fdf8a591fcfe6619876b866773986465182d3
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-11-02 14:51:59 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
-#from PyPDF2 import PdfReader
-from pypdf import PdfReader
+from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
